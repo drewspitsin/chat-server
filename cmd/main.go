ce82e3b0b1a4e56a02d0f136b5786e0cd789e1c9
drewspitsin <spitsin92.92@gmail.com>
2023-10-02 18:46:32 +0300

    First Homework v1.0

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	desc "github.com/drewspitsin/chat-server/pkg/chat_api_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedChat_API_V1Server
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	great_quit := make(chan os.Signal, 1)
+	signal.Notify(great_quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterChat_API_V1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	go func() {
+		if err = s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	<-great_quit
+	s.GracefulStop()
+	fmt.Println(color.YellowString("Auth_api server closed..."))
 }
